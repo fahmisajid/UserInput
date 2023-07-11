222a0868f383f445a5590067c72a64bede4bac98
fahmisajid <45916595+fahmisajid@users.noreply.github.com>
2023-07-11 13:49:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import pandas as pd
 import streamlit as st
 
-df = pd.read_csv('User_input.csv')
+@st.cache_data(ttl=600)
+def load_data(sheets_url):
+    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
+    return pd.read_csv(csv_url)
+
+df = load_data(st.secrets["public_gsheets_url"])
+
+# df = pd.read_csv('User_input.csv')
 
 sentence = st.text_input('Masukkan Kalimat:') 
 
