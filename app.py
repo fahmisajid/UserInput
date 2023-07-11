318d3838f7de17d28b9ff1f4d2176b35a2b0cbc3
fahmisajid <45916595+fahmisajid@users.noreply.github.com>
2023-07-11 15:22:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,33 @@
 import pandas as pd
 import streamlit as st
 
-df = pd.DataFrame(columns=["Questions", "Answers", "Response_Type"])
-df = pd.read_csv('User_input.csv')
+import pickle 
 
-sentence = st.text_input('Masukkan Kalimat:')
+# Clone the GitHub repository
+repo_url = 'https://github.com/fahmisajid/UserInput.git'
+local_path = 'UserInput'
+Repo.clone_from(repo_url, local_path)
 
-user_input = {'Questions': sentence, 'Answers': 'idk', 'Response_Type': 'bot'}
+# Specify the CSV file path
+csv_file_path = '/path/to/local/repository/UserInput.csv'
+
+df = pd.read_csv(path)
+
+sentence = st.text_input('Masukkan Kalimat:') 
+
+user_input = {'Questions':sentence, 'Answers': 'idk', 'Response_Type':'bot'}
 df_input = pd.DataFrame(user_input, index=[0])
 
 df_merge = pd.concat([df, df_input], ignore_index=True)
 
 st.write(df)
 
-df_merge.to_csv('User_input2.csv', index=False)
+# Write the updated DataFrame back to the CSV file
+df.to_csv(csv_file_path, index=False)
+
+# Commit and push the changes back to the GitHub repository
+repo = Repo(local_path)
+repo.git.add(csv_file_path)
+repo.index.commit('Added user input to UserInput.csv')
+origin = repo.remote('origin')
+origin.push()
