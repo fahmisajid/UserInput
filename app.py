a777e8409e8d800d787debe6c01d0c92e74e0ee0
fahmisajid <45916595+fahmisajid@users.noreply.github.com>
2023-07-11 13:39:44 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import streamlit as st
+
+df = pd.read_csv('User_input.csv')
+
+sentence = st.text_input('Masukkan Kalimat:') 
+
+user_input = {'Questions':sentence, 'Answers': 'idk', 'Response_Type':'bot'}
+df_input = pd.DataFrame(user_input, index=[0])
+
+df_merge = pd.concat([df, df_input], ignore_index=True)
+
+st.write(df_merge)
+
+# Save the merged DataFrame back to Excel
+df_merge.to_csv('User_input.csv', index=False)
