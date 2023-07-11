f284e0fcb30e00eef9ee7bd5d746f452a47d1fbf
fahmisajid <45916595+fahmisajid@users.noreply.github.com>
2023-07-11 15:45:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,31 @@
 import pandas as pd
 import streamlit as st
+import urllib.request
 
-from git import Repo
+# Define the file paths
+csv_url = 'https://github.com/fahmisajid/UserInput/blob/main/User_input.csv'
+csv_file_path = 'UserInput.csv'
 
-# Clone the GitHub repository
-repo_url = 'https://github.com/fahmisajid/UserInput.git'
-local_path = 'UserInput'
-Repo.clone_from(repo_url, local_path)
+# Download the CSV file from GitHub
+urllib.request.urlretrieve(csv_url, csv_file_path)
 
-# Specify the CSV file path
-csv_file_path = '/path/to/local/repository/UserInput.csv'
+# Read the CSV file into a DataFrame
+df = pd.read_csv(csv_file_path)
 
-df = pd.read_csv(path)
-
+# Get user input
 sentence = st.text_input('Masukkan Kalimat:') 
 
-user_input = {'Questions':sentence, 'Answers': 'idk', 'Response_Type':'bot'}
+# Create a dictionary with the user input
+user_input = {'Questions': sentence, 'Answers': 'idk', 'Response_Type': 'bot'}
+
+# Create a DataFrame from the user input dictionary
 df_input = pd.DataFrame(user_input, index=[0])
 
+# Concatenate the original DataFrame with the new input DataFrame
 df_merge = pd.concat([df, df_input], ignore_index=True)
 
-st.write(df)
+# Display the updated DataFrame in the Streamlit app
+st.write(df_merge)
 
 # Write the updated DataFrame back to the CSV file
 df_merge.to_csv(csv_file_path, index=False)
-
-# Commit and push the changes back to the GitHub repository
-repo = Repo(local_path)
-repo.git.add(csv_file_path)
-repo.index.commit('Added user input to UserInput.csv')
-origin = repo.remote('origin')
-origin.push()
