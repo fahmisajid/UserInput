d3b789d6e4fd36f112620edfe5698bcf6c93ce56
fahmisajid <45916595+fahmisajid@users.noreply.github.com>
2023-07-11 14:14:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
 import pandas as pd
 import streamlit as st
 
-df = pd.read_csv('User_input.csv')
+@st.cache(allow_output_mutation=True)
+def read_csv_file(file_path):
+    return pd.read_csv(file_path)
 
-sentence = st.text_input('Masukkan Kalimat:') 
+@st.cache(allow_output_mutation=True)
+def write_csv_file(df, file_path):
+    df.to_csv(file_path, index=False)
 
-user_input = {'Questions':sentence, 'Answers': 'idk', 'Response_Type':'bot'}
+file_path = 'User_input.csv'
+
+df = read_csv_file(file_path)
+
+sentence = st.text_input('Masukkan Kalimat:')
+
+user_input = {'Questions': sentence, 'Answers': 'idk', 'Response_Type': 'bot'}
 df_input = pd.DataFrame(user_input, index=[0])
 
 df_merge = pd.concat([df, df_input], ignore_index=True)
 
 st.write(df_merge)
 
-# Save the merged DataFrame back to Excel
-df_merge.to_csv('User_input.csv', index=False)
+write_csv_file(df_merge, file_path)
